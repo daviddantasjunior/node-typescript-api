89d8fd01d1a3ac9fa9bc317bd7f91f74fbd120f0
David Dantas <daviddantasjunior@gmail.com>
2021-07-13 21:53:23 -0300

    chore: Ensuring code style in Node js with prettier

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServe extends Server {
-
   constructor(private port = 3000) {
     super();
   }
